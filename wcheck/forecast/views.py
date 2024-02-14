1f1de0dcc3d41c5dc55ba8036c0a69fd4997c307
chistakaribsky <karybsky@gmail.com>
2024-02-15 01:59:06 +0200

    udated index method in  forecast/views.py and template forecast/index.html to provide forecast for whole list of cities in database

--- wcheck/forecast/views.py
+++ wcheck/forecast/views.py
@@ -1,37 +1,45 @@
 import requests
 from django.shortcuts import render
 
+from .models import City
+
 def index(request):
     appid = "8611d7daa87ebb4edc0667a80610d359"
-    city = "London"
-    geo_url = "https://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid=".format(city) + appid
-    geo_response = requests.get(geo_url)
-    context = {}
     
-    if geo_response.status_code == 200:
-        geo_data = geo_response.json()
-        if geo_data:
-            lat = geo_data[0]['lat']
-            lon = geo_data[0]['lon']
+    cities = City.objects.all()
+    
+
+    all_cities = []
+    
+    for city in cities:
+        
+        geo_url = f"https://api.openweathermap.org/geo/1.0/direct?q={city.name}&limit=1&appid=" + appid
+        geo_response = requests.get(geo_url)
+        
+        if geo_response.status_code == 200:
+            geo_data = geo_response.json()
             
-            weather_url = "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=metric&appid={}".format(lat, lon, appid)
-            
-            weather_response = requests.get(weather_url)
-            
-            if weather_response.status_code == 200:
-                weather_data = weather_response.json()
-                api_output = {
-                    'city': city,
-                    'temp': weather_data['main']['temp'],
-                    'icon': weather_data['weather'][0]['icon'],
-                }
-                context = {'result': api_output}
+            if geo_data:
+                lat = geo_data[0]['lat']
+                lon = geo_data[0]['lon']
+                weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={appid}"
+                open_weather_api_response = requests.get(weather_url)
                 
+                if open_weather_api_response.status_code == 200:
+                    api_output = open_weather_api_response.json()
+                    weather_data = {
+                        'city': city.name,
+                        'temp': api_output['main']['temp'],
+                        'icon': api_output['weather'][0]['icon'],
+                    }
+                    all_cities.append(weather_data)
+                else:
+                    print("Failed to fetch weather data:", open_weather_api_response.status_code)
             else:
-                print("Failed to fetch weather data:", weather_response.status_code)
+                print("No data found for the specified city:", city.name)
         else:
-            print("No data found for the specified city:", city)
-    else:
-        print("Failed to fetch geocoding data:", geo_response.status_code)
+            print("Failed to fetch geocoding data:", geo_response.status_code)
     
+    context = {'results': all_cities}
+            
     return render(request, 'forecast/index.html', context)
