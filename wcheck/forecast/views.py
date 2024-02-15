dde64ed48c147a68523e413cdbfe69646073d22f
chistakaribsky <karybsky@gmail.com>
2024-02-15 18:54:23 +0200

    refactored forecast/views.py and hide appid into wcheck/.env file
    I know that there are still old commits where appid is explicitly displayed in the code, but I hadn't delete them because it's just a demo app

--- wcheck/forecast/views.py
+++ wcheck/forecast/views.py
@@ -1,48 +1,53 @@
+import os
+from dotenv import load_dotenv
 import requests
 from django.shortcuts import render
 from .models import City
 from .forms import CityForm
 
-def index(request):
-    appid = "8611d7daa87ebb4edc0667a80610d359"
+def fetch_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Failed to fetch data from", url, "Status Code:", response.status_code)
+        return None
+
+def fetch_weather_data(city_name):
+    load_dotenv()
+    WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+    geo_url = f"https://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid=" + WEATHER_API_KEY
+    geo_data = fetch_data(geo_url)
     
-    if(request.method == "POST"):
+    if geo_data:
+        lat = geo_data[0]['lat']
+        lon = geo_data[0]['lon']
+        weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={WEATHER_API_KEY}"
+        return fetch_data(weather_url)
+    else:
+        print("No data found for the specified city:", city_name)
+        return None
+
+def index(request):
+    if request.method == "POST":
         form = CityForm(request.POST)
-        form.save()
-        
-    form = CityForm()
+        if form.is_valid():
+            form.save()
+    else:
+        form = CityForm()
     
     cities = City.objects.all()
     all_cities = []
     
     for city in cities:
-        
-        geo_url = f"https://api.openweathermap.org/geo/1.0/direct?q={city.name}&limit=1&appid=" + appid
-        geo_response = requests.get(geo_url)
-        
-        if geo_response.status_code == 200:
-            geo_data = geo_response.json()
-            
-            if geo_data:
-                lat = geo_data[0]['lat']
-                lon = geo_data[0]['lon']
-                weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={appid}"
-                open_weather_api_response = requests.get(weather_url)
-                
-                if open_weather_api_response.status_code == 200:
-                    api_output = open_weather_api_response.json()
-                    weather_data = {
-                        'city': city.name,
-                        'temp': api_output['main']['temp'],
-                        'icon': api_output['weather'][0]['icon'],
-                    }
-                    all_cities.append(weather_data)
-                else:
-                    print("Failed to fetch weather data:", open_weather_api_response.status_code)
-            else:
-                print("No data found for the specified city:", city.name)
-        else:
-            print("Failed to fetch geocoding data:", geo_response.status_code)
+        weather_data = fetch_weather_data(city.name)
+        if weather_data:
+            weather_info = {
+                'city': city.name,
+                'temp': weather_data['main']['temp'],
+                'icon': weather_data['weather'][0]['icon'],
+            }
+            all_cities.append(weather_info)
     
     context = {"results": all_cities, "form": form}
             
